5f756bab0e69ce682ad7b514d5fd8542da5a500f
Santiago Orozco <everardoorozcocarmona@gmail.com>
2023-11-09 09:01:31 -0500

    se corrigio el sql de la base de datos pa que no de errores al importarlo

--- db/db_inventario.sql
+++ db/db_inventario.sql
@@ -1,115 +1,132 @@
--- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1    Database: db_inventario
--- ------------------------------------------------------
--- Server version	8.1.0
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 09-11-2023 a las 14:59:35
+-- Versión del servidor: 8.0.32
+-- Versión de PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Table structure for table `contros_inventario`
+-- Base de datos: `db_inventario`
 --
 
-DROP TABLE IF EXISTS `contros_inventario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `contros_inventario`
+--
+
 CREATE TABLE `contros_inventario` (
-  `pk_control` bigint NOT NULL AUTO_INCREMENT,
+  `pk_control` bigint NOT NULL,
   `salida` int NOT NULL,
   `entrada` int NOT NULL,
   `fecha_salida` date NOT NULL,
   `hora_salida` time NOT NULL,
   `fecha_entrada` date NOT NULL,
   `hora_entrada` time NOT NULL,
-  `product_fk` bigint NOT NULL,
-  PRIMARY KEY (`pk_control`),
-  KEY `fk_producto` (`product_fk`),
-  CONSTRAINT `fk_producto` FOREIGN KEY (`product_fk`) REFERENCES `productos` (`pk_prod`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `product_fk` bigint NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `contros_inventario`
+-- Estructura de tabla para la tabla `productos`
 --
 
-LOCK TABLES `contros_inventario` WRITE;
-/*!40000 ALTER TABLE `contros_inventario` DISABLE KEYS */;
-/*!40000 ALTER TABLE `contros_inventario` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `productos`
---
-
-DROP TABLE IF EXISTS `productos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `productos` (
-  `pk_prod` bigint NOT NULL AUTO_INCREMENT,
-  `referencia` char(80) COLLATE utf8mb4_general_ci NOT NULL,
-  `nombre_producto` char(80) COLLATE utf8mb4_general_ci NOT NULL,
-  `descripcion` char(80) COLLATE utf8mb4_general_ci NOT NULL,
-  `foto_product` varchar(300) COLLATE utf8mb4_general_ci NOT NULL,
-  `ubicacionBodega` varchar(60) COLLATE utf8mb4_general_ci NOT NULL,
+  `pk_prod` bigint NOT NULL,
+  `referencia` char(80) NOT NULL,
+  `nombre_producto` char(80) NOT NULL,
+  `descripcion` char(80) NOT NULL,
+  `foto_product` varchar(300) NOT NULL,
+  `ubicacionBodega` varchar(60) NOT NULL,
   `precio_product` double(22,0) NOT NULL,
   `existencia` int NOT NULL,
   `stock` int NOT NULL,
-  `estado` int NOT NULL,
-  PRIMARY KEY (`pk_prod`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `estado` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `productos`
+-- Estructura de tabla para la tabla `usuarios`
 --
 
-LOCK TABLES `productos` WRITE;
-/*!40000 ALTER TABLE `productos` DISABLE KEYS */;
-/*!40000 ALTER TABLE `productos` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `usuarios`
---
-
-DROP TABLE IF EXISTS `usuarios`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuarios` (
-  `pk_user` bigint NOT NULL AUTO_INCREMENT,
-  `usuario` char(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `password` char(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `foto_user` varchar(400) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `rol` int NOT NULL,
-  PRIMARY KEY (`pk_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `pk_user` bigint NOT NULL,
+  `usuario` char(250) NOT NULL,
+  `password` char(50) NOT NULL,
+  `foto_user` varchar(400) NOT NULL,
+  `rol` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `usuarios`
+-- Índices para tablas volcadas
 --
 
-LOCK TABLES `usuarios` WRITE;
-/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+--
+-- Indices de la tabla `contros_inventario`
+--
+ALTER TABLE `contros_inventario`
+  ADD PRIMARY KEY (`pk_control`),
+  ADD KEY `fk_product` (`product_fk`);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`pk_prod`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`pk_user`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `contros_inventario`
+--
+ALTER TABLE `contros_inventario`
+  MODIFY `pk_control` bigint NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `pk_prod` bigint NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `pk_user` bigint NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `contros_inventario`
+--
+ALTER TABLE `contros_inventario`
+  ADD CONSTRAINT `fk_product` FOREIGN KEY (`product_fk`) REFERENCES `productos` (`pk_prod`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-11-08 17:53:34
