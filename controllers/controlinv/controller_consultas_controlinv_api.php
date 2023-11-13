ba6a7218be4fc83f38f8106227c41175cc3603af
Santiago Orozco <everardoorozcocarmona@gmail.com>
2023-11-12 20:00:03 -0500

    se corrigio un pequeño error de sintaxis al listar controlinv

--- controllers/controlinv/controller_consultas_controlinv_api.php
+++ controllers/controlinv/controller_consultas_controlinv_api.php
@@ -1,43 +1,46 @@
 <?php
 session_start();
-require_once( "../../models/models_admin.php");
-class ConsultasDB extends DBConfig {
-    					
-	function consulta_generales($sql){
+require_once("../../models/models_admin.php");
+class ConsultasDB extends DBConfig
+{
+
+	function consulta_generales($sql)
+	{
 		$this->config();
-		$this->conexion(); 
-		  
-  		$records = $this->Consultas($sql);		 		  		  		  
+		$this->conexion();
 
-  		$this->close();		
-		return $records;				
+		$records = $this->Consultas($sql);
+
+		$this->close();
+		return $records;
 	}
-
 }
 
 
 /**
-* IMPLEMENTACION DE ACCESO A CONSULTAS PARA PROTEGER MAS LA VISTA
-*/
+ * IMPLEMENTACION DE ACCESO A CONSULTAS PARA PROTEGER MAS LA VISTA
+ */
 class ExtraerDatos extends ConsultasDB
 {
-			
+
 
 	// ****************************************************************************
 	// Agregue aqui debajo el resto de Funciones - Se ha dejado  Listado y detalle
 	// ****************************************************************************
-    //MUESTRA LISTADO DE EMPLEADOS
-	function listadoControlinvetario($start=0, $regsCant = 0){
-		$sql = "SELECT * FROM contros_invetario";
-		if ($regsCant > 0 )
-			 $sql = "SELECT * from contros_invetario $start,$regsCant";
-		$lista = $this->consulta_generales($sql);	
+	//MUESTRA LISTADO DE EMPLEADOS
+	function listadoControlinvetario($start = 0, $regsCant = 0)
+	{
+		$sql = "SELECT * FROM contros_inventario";
+		if ($regsCant > 0)
+			$sql = "SELECT * from contros_inventario $start,$regsCant";
+		$lista = $this->consulta_generales($sql);
 		return $lista;
 	}
 	// DETALLE DE EMPLEADOS SELECICONADA SEGUN ID
-	function controlDetalle($idu){
+	function controlDetalle($idu)
+	{
 		$sql = "SELECT * from contros_inventario where pk_control=$idu ";
-		$lista = $this->consulta_generales($sql);	
+		$lista = $this->consulta_generales($sql);
 		return $lista;
 	}
 
@@ -48,27 +51,29 @@ class ExtraerDatos extends ConsultasDB
 	// 	return $lista;
 	// }
 
-	function saveControlinventario($salida,$fkProduc){
+	function saveControlinventario($salida, $fkProduc)
+	{
 		$objDBO = new DBConfig();
-        $objDBO->config();
-        $objDBO->conexion();
+		$objDBO->config();
+		$objDBO->conexion();
 
 		$entrada = 0;
 		$fecha_salida = date('Y-m-d');
 		$hora_salida = date('H:i:s');
 		$fecha_entrada = date('Y-m-d');
 		$hora_entrada = date('H:i:s');
-		
+
 		$ejecucion = $objDBO->operaciones("INSERT INTO contros_inventario(salida, entrada, fecha_salida,  hora_salida, fecha_entrada, hora_entrada, product_fk)
 														values($salida, $entrada, '$fecha_salida', '$hora_salida', '$fecha_entrada', '$hora_entrada', $fkProduc)");
 
 		return $ejecucion;
 	}
 
-	function updateUser($id,$usuario,$rol){
+	function updateUser($id, $usuario, $rol)
+	{
 		$objDBO = new DBConfig();
-        $objDBO->config();
-        $objDBO->conexion();
+		$objDBO->config();
+		$objDBO->conexion();
 		// $passw = sha1($password);
 
 		$ejecucion = $objDBO->operaciones("UPDATE usuarios SET usuario='$usuario', rol=$rol WHERE pk_user=$id");
@@ -76,10 +81,11 @@ class ExtraerDatos extends ConsultasDB
 		return $ejecucion;
 	}
 
-	function updatePasswordUser($id,$password){
+	function updatePasswordUser($id, $password)
+	{
 		$objDBO = new DBConfig();
-        $objDBO->config();
-        $objDBO->conexion();
+		$objDBO->config();
+		$objDBO->conexion();
 		// $passw = sha1($password);
 
 		$ejecucion = $objDBO->operaciones("UPDATE usuarios SET password='$password' WHERE pk_user=$id");
@@ -87,10 +93,11 @@ class ExtraerDatos extends ConsultasDB
 		return $ejecucion;
 	}
 
-	function updateUserfoto($id,$usuario,$foto,$rol){
+	function updateUserfoto($id, $usuario, $foto, $rol)
+	{
 		$objDBO = new DBConfig();
-        $objDBO->config();
-        $objDBO->conexion();
+		$objDBO->config();
+		$objDBO->conexion();
 		// $passw = sha1($password);
 
 		$ejecucion = $objDBO->operaciones("UPDATE usuarios SET usuario='$usuario', foto_user='$foto', rol=$rol WHERE pk_user=$id");
@@ -98,22 +105,14 @@ class ExtraerDatos extends ConsultasDB
 		return $ejecucion;
 	}
 
-	function deleteUser($id){
+	function deleteUser($id)
+	{
 		$objDBO = new DBConfig();
-        $objDBO->config();
-        $objDBO->conexion();
+		$objDBO->config();
+		$objDBO->conexion();
 
 		$ejecucion = $objDBO->operaciones("DELETE FROM usuarios WHERE pk_user=$id");
 
 		return $ejecucion;
 	}
-		
-		
-
-       
-
-
-	
-	
-	
 }//fin CLASE
