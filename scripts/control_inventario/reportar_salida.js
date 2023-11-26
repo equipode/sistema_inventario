67612dbd8f04624c89f0a24ce255d1f04e1d31e4
Robert López oviedo <ingenierolopez2022@gmail.com>
2023-11-26 12:23:25 -0500

    se realisó la tabla en html para mostrar datos

--- scripts/control_inventario/reportar_salida.js
+++ scripts/control_inventario/reportar_salida.js
@@ -4,5 +4,19 @@ const contenido = document.getElementById('table_salida');
 fetch(`${baseUrl}/controlinv/controlinv_api.php`)
 .then(response => response.json())
 .then(resp => {
-console.log(resp);    
+    let content = "";
+
+resp.data.forEach(element => {
+
+content += `<tr>`;
+content += `<td>${element.hora_egreso}`;
+content += `<td>${element.fecha_egreso}`;
+content += `<td>${element.cantidad_salida}`;
+content += `<td>${element.productofk}`;
+content += `<td>${element.pk_cont}`;
+content += `<td>${element.pk_cont}`;
+content += `<td>${element.pk_cont}`;
+content += `</tr>`;
+});
+contenido.innerHTML = content;
 })
